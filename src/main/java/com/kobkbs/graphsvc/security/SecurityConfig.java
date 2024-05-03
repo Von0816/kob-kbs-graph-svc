9757b8648a87ec3cffa3fb2239612e902b66376b
Boonlert Randy <boonlertrandy@gmail.com>
2024-05-03 22:42:59 +0800

    implemented api key authentication for restful api request

--- src/main/java/com/kobkbs/graphsvc/security/SecurityConfig.java
+++ src/main/java/com/kobkbs/graphsvc/security/SecurityConfig.java
@@ -1,30 +1,28 @@
-// package com.kobkbs.graphsvc.security;
-//
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.http.HttpMethod;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.http.SessionCreationPolicy;
-// import org.springframework.security.core.userdetails.User;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-// import org.springframework.security.web.SecurityFilterChain;
-//
-// @Configuration
-// @EnableWebSecurity
-// public class SecurityConfig {
-//
-//   @Bean
-//   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//
-//     http.oauth2Client().and().oauth2Login().tokenEndpoint().and().userInfoEndpoint();
-//
-//     http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
-//
-//     http.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.GET, "/*").permitAll().anyRequest().hasRole("ADMIN")).formLogin(form -> form.loginPage("/login").permitAll()).logout(logout -> logout.permitAll());
-//
-//     return http.build();
-//   }
-// }
+package com.kobkbs.graphsvc.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+          .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.requestMatchers("/**").authenticated())
+          .httpBasic(Customizer.withDefaults())
+          .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+          .addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
